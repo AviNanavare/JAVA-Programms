5ef2f17e5df810bdd9c80400d895fda55cdfb1de
AviNanavare <60835528+AviNanavare@users.noreply.github.com>
2020-02-09 11:51:47 +0530

    File Uploaded
    SET-1

---
+++ SpringSeason.java
@@ -0,0 +1,26 @@
+package week1;
+import java.util.Scanner;
+
+public class SpringSeason {
+	public void springVala(int M , int D)
+	{
+		if(M>3)
+	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a Month and Day : ");
+		int M=scanner.nextInt();
+		int D=scanner.nextInt();
+		
+		if(M==3)
+		{
+			if(D)
+			
+			
+			
+		}
+		
+		scanner.close();
+		}
+
+}
